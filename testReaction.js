511481791d4c4bf37ef271c263b777be7b446e4a
Raksha Gupta <guptaraksha712@gmail.com>
2020-06-06 00:51:56 +0530

    Add files via upload
    Java Script file uploaded.

---
+++ testReaction.js
@@ -0,0 +1,27 @@
+  var startTime = new Date().getTime();
+        function appearShape() {
+            var top = Math.random() * 400;
+            var left = Math.random() * 500;
+            var radius = Math.random() * 100;
+            var color = Math.floor(Math.random() * 16777215).toString(16);
+            document.getElementById("shape").style.display = "block";
+            document.getElementById("shape").style.top = top + "px";
+            document.getElementById("shape").style.left = left + "px";
+            document.getElementById("shape").style.borderRadius = radius + "px";
+            document.getElementById("shape").style.backgroundColor = "#" + color;
+            startTime = new Date().getTime();
+        }
+        function delayAppear() {
+
+            setTimeout(appearShape, Math.random() * 2000);
+        }
+        delayAppear();
+        document.getElementById("shape").onclick = function () {
+            var endTime = new Date().getTime();
+            document.getElementById("shape").style.display = "none";
+            var calTime = (endTime - startTime) / 1000
+            document.getElementById("timerTime").innerHTML = calTime + "s";
+
+            delayAppear();
+
+        }
